99ecff29dda6e10b0cbf1ab7688da24d3501eaaf
cedd <albuerocedrick@gmail.com>
2025-07-03 22:16:29 +0800

    added arithmetic operators

---
+++ 5.Operators/arithmetic.py
@@ -0,0 +1,11 @@
+#arithmetic operators
+x = 10
+y = 6
+
+print(x + y) #addition
+print(x - y) #subtraction
+print(x * y) #multiplication
+print(x / y) #division
+print(x % y) #modulos (remainder when divided)
+print(x ** y) #exponential(10 raise to the power of 6)
+print(x // y) #floor division (normal division, but returns whole number)

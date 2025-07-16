2aabd854ed85cd295777a39d9cc7113c833f66d3
albuerocedrick <albuerocedrick11@gmail.com>
2025-07-16 10:57:25 +0800

    finished code for forloop.py

--- fundamentals/8.Loops/forloop.py
+++ fundamentals/8.Loops/forloop.py
@@ -28,3 +28,41 @@ for i in fruits:
     if i == "apple": #if the item is apple, it will skip the other code below then continue to the next iteration
         continue
     print(i)
+
+print()
+
+#using range() in for loop
+for i in range(6): #this will loop starting from 0-6
+    print(i)
+
+print()
+
+#if you want to start at a specific number, you can specify it like this range(starting point, end point)
+for i in range(3, 6): #will loop from 3-5
+    print(i) 
+
+print()
+
+#the default increment of for loop is 1 but you can also specify it by adding another parameter
+#like this range (starting point, end point, increment value)
+for i in range(1, 31, 2): #this will increment by 2
+    print(i)
+
+print()
+
+#using else keyword in for loop
+#else keyword will not run if it is stopped by a break statement
+for i in range(6):
+    print(i)
+else:
+    print("loop is done!")
+
+print()
+
+#nested for loop
+firstname = ["malaking", "maliit na", "masarap na"]
+lastname = ["mansanas", "saging", "bayabas"]
+
+for i in firstname:
+    for x in lastname:
+        print(i, x)
